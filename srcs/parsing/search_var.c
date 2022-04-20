0c9031e78a8ac4692efda06c05fce47e1a2d1217
RodelCC <byeonggukson@gmail.com>
2022-04-20 17:30:36 +0900

    fix 24767, $

--- srcs/parsing/search_var.c
+++ srcs/parsing/search_var.c
@@ -1,13 +1,25 @@
 #include "minishell.h"
 
-static char	*dup_replace_var(t_env_node *node)
+static char	*dup_replace_var(t_env_list *list, char **start, char *end, bool escape)
 {
+	char		*temp;
+	t_env_node	*node;
+
+	if (escape == true)
+	{
+		--(*start);
+		return (ft_strdup("$"));
+	}
+	temp = ft_substr(*start, 1, end - *start - 1);
+	node = get_env_node(list, temp);
+	if (temp != NULL)
+		free(temp);
 	if (node == NULL)
 		return (ft_strdup(""));
 	return (ft_strdup(node->value));
 }
 
-static char	*expand_input(char **input, char *var_start, t_env_list *env)
+static char	*expand_input(char **input, char *var_start, t_env_list *env, bool escape)
 {
 	char	*var_end;
 	char	*replace_var;
@@ -17,13 +29,10 @@ static char	*expand_input(char **input, char *var_start, t_env_list *env)
 	var_end = var_start + 1;
 	while (*var_end && ft_strchr("\"\'<>| $", *var_end) == NULL)
 		++var_end;
-	ret = ft_substr(var_start, 1, var_end - var_start - 1);
-	replace_var = dup_replace_var(get_env_node(env, ret));
-	if (replace_var == NULL)
-		return (NULL);
+	replace_var = dup_replace_var(env, &var_start, var_end, escape);
+	ft_assert(replace_var != NULL, "ERROR : leak resource in expand_input()");
 	replace_var_len = ft_strlen(*input) - (var_end - var_start + 1) \
 	+ ft_strlen(replace_var);
-	free(ret);
 	ret = ft_calloc(1, sizeof(char) * (replace_var_len + 1));
 	ft_assert(ret != NULL, "ERROR : leak resource in expand_input()");
 	ft_strncpy(ret, *input, var_start - *input);
@@ -51,12 +60,10 @@ static char	*jump_word(char *input)
 	$만 들어오면 \$와 동일하게 처리해야한다.
 	sd
 */
-void	search_var(char **input, t_env_list *env)
+void	search_var(char **input, t_env_list *env, bool quote_flag)
 {
 	char	*iter;
-	bool	quote_flag;
 
-	quote_flag = false;
 	if (*input == NULL)
 		return ;
 	iter = *input;
@@ -68,10 +75,12 @@ void	search_var(char **input, t_env_list *env)
 			iter = ft_strchr(iter + 1, '\'');
 		else if (*iter == '<' && *(iter + 1) == '<' && quote_flag == false)
 			iter = jump_word(iter += 2);
-		else if (*iter == '$' && \
-				(iter == *input || *(iter - 1) != '\\'))
+		else if (*iter == '$')
 		{
-			iter = expand_input(input, iter, env);
+			if (iter != *input && *(iter - 1) == '\\')
+				iter = expand_input(input, iter, env, true);
+			else
+				iter = expand_input(input, iter, env, false);
 			continue;
 		}
 		++iter;
