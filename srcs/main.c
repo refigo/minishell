1f7b1cb36fdb5ac9ed76b2b03a49ab75031de517
RodelCC <byeonggukson@gmail.com>
2022-04-25 11:49:56 +0900

    fix leak char *input

--- srcs/main.c
+++ srcs/main.c
@@ -8,21 +8,23 @@ int main(int argc, char **argv, char **envp)
 	t_ast		*syntax;
 	char *temp;
 
+	input = NULL;
 	init_info(&info);
 	info.envp = envp;	// mgo: set envp into info
 	make_env_list(info.unordered_env, argc, argv, envp);
-	{
-		t_env_list	*sorted;
-		sorted = env_sort_copy_env(info.unordered_env);
-		printf("unordered env----------------------------\n");
-		iter_env_list(info.unordered_env, print_node);
-		printf("ordered env-------------------------------\n");
-		iter_env_list(sorted, print_node);
-		printf("-----------------------------------------\n");
-		return (0);
-	}
+	// {
+	// 	t_env_list	*sorted;
+	// 	sorted = env_sort_copy_env(info.unordered_env);
+	// 	printf("unordered env----------------------------\n");
+	// 	iter_env_list(info.unordered_env, print_node);
+	// 	printf("ordered env-------------------------------\n");
+	// 	iter_env_list(sorted, print_node);
+	// 	printf("-----------------------------------------\n");
+	// 	return (0);
+	// }
 	while (true)
 	{
+		ft_free(&input);
 		refresh_info(&info);
 		input = readline("minishell$ ");
 		if (!ft_strlen(input) || is_empty(input))
